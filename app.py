c677e984fe9b13b4c3d22e62447bd73c879ff4db
Amit <asemwal125@gmail.com>
2024-10-09 16:45:13 +0530

    changes in app.py to check github action ci/cd

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Welcome to Flask Web App!"
+    return "Welcome to Flask Web App11!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
